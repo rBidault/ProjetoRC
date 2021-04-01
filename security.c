e2deac8d495922d2505b1676505232f1e645d623
rBidault <81481720+rBidault@users.noreply.github.com>
2021-04-01 19:02:52 +0100

    Add files via upload

---
+++ security.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+#define BUF_SIZE 1024
+
+void erro(char *msg);
+
+int main() {
+  char endServer[100], buffer[BUF_SIZE];
+  int fd, client, nread;
+  struct sockaddr_in addr, client_addr;
+  int client_addr_size;
+  struct hostent *hostPtr;
+  char login[20], pw[20];
+  char app[] = "security";
+  int check=1;
+  
+
+  bzero((void *) &addr, sizeof(addr));
+  addr.sin_family = AF_INET;
+  addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  addr.sin_port = htons(9000);
+
+
+  if((fd = socket(AF_INET,SOCK_STREAM,0)) == -1){
+	  erro("socket");
+  }
+  if( connect(fd,(struct sockaddr *)&addr,sizeof (addr)) < 0){
+	  erro("Connect");
+  }
+  printf("Bem vindo à aplicação do Seguranaa de Saude.\n");
+  write(fd, app, strlen(app));
+  memset(buffer,0,strlen(buffer));
+	printf("Introduza o seu Login: ");
+  fgets(login,20,stdin);
+  login[strcspn(login, "\n")] = 0;
+  printf("Introduza a sua Password: ");
+  fgets(pw,20,stdin);
+  pw[strcspn(pw, "\n")] = 0;
+  strcat(buffer,login);
+	strcat(buffer," ");
+	strcat(buffer,pw);
+	write(fd, buffer, strlen(buffer));
+  memset(buffer,0,strlen(buffer));
+  nread = read(fd, buffer, BUF_SIZE-1);		
+	buffer[nread] = '\0';
+
+	check=strcmp(buffer,"0");
+  if (check==0){
+    erro("Utilizador não existe!");
+  }
+  
+	memset(buffer,0,strlen(buffer)); 
+	
+	printf("\nSeguranca na Saude: Seguranca de Saude\n");
+
+
+  fflush(stdout);
+  close(fd);
+}
+
+void erro(char *msg)
+{
+	printf("Erro: %s\n", msg);
+	exit(-1);
+}
---
+++ server.c
@@ -0,0 +1,182 @@
+//LIBRARIES
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <netdb.h>
+#include <string.h>
+
+//STATIC VARIABLES
+#define SERVER_PORT     9000
+#define BUF_SIZE	1024
+
+//FUNCTIONS
+void process_client(int fd);
+void health_app();
+void security_app();
+void admin_app();
+void erro(char *msg);
+
+//GLOBAL VARIABLES
+FILE *text;
+int s=1, h=1, a=1;
+int nread = 0;
+char buffer[BUF_SIZE];
+
+int main() {
+  int fd, client;
+  struct sockaddr_in addr, client_addr;
+  int client_addr_size;
+  addr.sin_family      = AF_INET;
+  addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  addr.sin_port        = htons(SERVER_PORT);
+
+  if ( (fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
+	erro("na funcao socket");
+  if ( bind(fd,(struct sockaddr*)&addr,sizeof(addr)) < 0)
+	erro("na funcao bind");
+  if( listen(fd, 5) < 0) 
+	erro("na funcao listen");
+  
+  int nclients=0;
+    
+  while (1) {
+    client_addr_size = sizeof(client_addr);
+    client = accept(fd,(struct sockaddr *)&client_addr,&client_addr_size);
+      nclients++;
+    if (client > 0) {
+      if (fork() == 0) {
+        close(fd);
+        process_client(client);
+        exit(0);
+      }
+    close(client);
+    }
+  }
+  return 0;
+}
+
+
+void process_client(int client_fd){
+  nread = read(client_fd, buffer, BUF_SIZE-1);	
+	buffer[nread] = '\0';
+  h=strcmp(buffer,"health");
+  s=strcmp(buffer,"security");
+  a=strcmp(buffer,"admin");
+  memset(buffer,0,BUF_SIZE); 
+
+  if (h==0){
+    health_app(client_fd);
+  }
+  if (s==0){
+    security_app(client_fd);
+  }
+  if (a==0){
+    admin_app(client_fd);
+  }
+	fflush(stdout);
+	close(client_fd);
+    
+}
+
+void health_app(int client_fd){
+  text = fopen ("RHealth.txt","r");
+  char *userinfo=NULL;
+  size_t len=0;
+  char user[BUF_SIZE];
+  int check=1;
+  char msg[BUF_SIZE];
+	if(text == NULL) {
+    erro("Abertura do ficheiro");
+  }
+  memset(user,0,BUF_SIZE); 
+  nread = read(client_fd, buffer, BUF_SIZE-1);	
+	buffer[nread] = '\0';
+  strcat(user,buffer);
+  memset(buffer,0,BUF_SIZE); 
+  while ((getline(&userinfo, &len, text))!= -1){
+    if(strstr(userinfo,user)){
+      break;
+    }
+    userinfo=NULL;
+  }
+  userinfo[strcspn(userinfo, "\n")] = 0;
+  check=strcmp(userinfo,user);     
+  if(check!=0){
+    strcat(msg,"0");
+    write(client_fd, msg, strlen(msg));
+    exit(-1);
+  }
+  printf("Nice");
+
+  
+  fclose(text);
+}
+
+void security_app(int client_fd){
+  text = fopen ("RSecurity.txt","r");
+  char *userinfo=NULL;
+  size_t len=0;
+  char user[BUF_SIZE];
+  int check=1;
+  char msg[BUF_SIZE];
+	if(text == NULL) {
+    erro("Abertura do ficheiro");
+  }
+  memset(user,0,BUF_SIZE); 
+  nread = read(client_fd, buffer, BUF_SIZE-1);	
+	buffer[nread] = '\0';
+  strcat(user,buffer);
+  memset(buffer,0,BUF_SIZE); 
+  while ((getline(&userinfo, &len, text))!= -1){
+    if(strstr(userinfo,user)){
+      break;
+    }
+    userinfo=NULL;
+  }
+  userinfo[strcspn(userinfo, "\n")] = 0;
+  check=strcmp(userinfo,user);     
+  if(check!=0){
+    strcat(msg,"0");
+    write(client_fd, msg, strlen(msg));
+    exit(-1);
+  }  
+  fclose(text);
+}
+void admin_app(int client_fd){
+  text = fopen ("RAdmin.txt","r");
+  char *userinfo=NULL;
+  size_t len=0;
+  char user[BUF_SIZE];
+  int check=1;
+  char msg[BUF_SIZE];
+	if(text == NULL) {
+    erro("Abertura do ficheiro");
+  }
+  memset(user,0,BUF_SIZE); 
+  nread = read(client_fd, buffer, BUF_SIZE-1);	
+	buffer[nread] = '\0';
+  printf("%s", buffer);
+  strcat(user,buffer);
+  memset(buffer,0,BUF_SIZE); 
+  while ((getline(&userinfo, &len, text))!= -1){
+    if(strstr(userinfo,user)){
+      break;
+    }
+    userinfo=NULL;
+  }
+  userinfo[strcspn(userinfo, "\n")] = 0;
+  check=strcmp(userinfo,user);     
+  if(check!=0){
+    strcat(msg,"0");
+    write(client_fd, msg, strlen(msg));
+    exit(-1);
+  }
+  fclose(text);
+}
+void erro(char *msg){
+	printf("Erro: %s\n", msg);
+	exit(-1);
+}
