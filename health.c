43452bf2b03651f2edd4345dad91e6a5dc39ae9c
rBidault <81481720+rBidault@users.noreply.github.com>
2021-05-01 19:45:08 +0100

    Update health.c

--- health.c
+++ health.c
@@ -11,29 +11,27 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 
+#define SERVER_PORT     9000
 #define BUF_SIZE 1024
-int op;
+char buffer[BUF_SIZE];
+char app[] = "health";
+char login[20] = "", pw[20] ="";
+int fd, nread,op, check=1;
+struct sockaddr_in addr;
+struct hostent *hostPtr;
 
 void erro(char *msg);
 void menu(int fd, char login[20]);
+void signIN(int fd);
 void signUP(int fd);
+void frontPAGE(int fd);
 
 int main() {
-  char buffer[BUF_SIZE];
-  int fd, client, nread;
-  struct sockaddr_in addr;
-  struct hostent *hostPtr;
-  char login[20], pw[20];
-  char app[] = "health";
-  int check=1;
- 
   
-
   bzero((void *) &addr, sizeof(addr));
   addr.sin_family = AF_INET;
   addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-  addr.sin_port = htons(9000);
-
+  addr.sin_port = htons(SERVER_PORT);
 
   if((fd = socket(AF_INET,SOCK_STREAM,0)) == -1){
 	  erro("socket");
@@ -41,11 +39,15 @@ int main() {
   if( connect(fd,(struct sockaddr *)&addr,sizeof (addr)) < 0){
 	  erro("Connect");
   }
+  write(fd, app, strlen(app));//manda ao servidor a identificação da app
+  frontPAGE(fd);
+  fflush(stdout);
+  close(fd);
+}
+void frontPAGE(int fd){
   system("clear");
   printf("Bem vindo à aplicação do Profissional de Saúde.\n");
-  write(fd, app, strlen(app));//manda ao servidor a identificação da app
   memset(buffer,0,strlen(buffer));
-  
   printf("\n1- Entrar\n2- Criar Conta\n0- Sair\n");
   printf("Escolha uma da opções: ");
 	scanf("%d", &op);
@@ -53,67 +55,59 @@ int main() {
     printf("Escolha Inválida. Reintroduza: ");
     scanf("%d", &op);
   }
+  getchar();
   switch(op){
     case 1:
-      system("clear");
-      printf("Introduza o seu Login: ");//pede login
-      fgets(login,20,stdin);
-      login[strcspn(login, "\n")] = 0;
-      printf("\nIntroduza a sua Password: ");//pede pass
-      fgets(pw,20,stdin);
-      pw[strcspn(pw, "\n")] = 0;
-      strcat(buffer,login);//junta login+pass numa str
-  	  strcat(buffer," ");
-  	  strcat(buffer,pw);
-  	  write(fd, buffer, strlen(buffer));//envia ao servidor
-      memset(buffer,0,strlen(buffer));
-      nread = read(fd, buffer, BUF_SIZE-1); //recebe verificação do login pelo servidor
-  	  buffer[nread] = '\0';
-
-	    check=strcmp(buffer,"0");
-      if (check==0){
-        erro("Utilizador não existe!");
-      }
-  
-	    memset(buffer,0,strlen(buffer));
-      menu(fd,login);
+      signIN(fd);
     case 2:
       signUP(fd);
     case 0:
-      return 0;
+      return;
   }
-
-  fflush(stdout);
-  close(fd);
 }
+
+void signIN(int fd){
+  system("clear");
+  printf("Introduza o seu Nome de Utilizador: ");//pede login
+  fgets(login,20,stdin);
+  login[strcspn(login, "\n")] = 0;
+  printf("Introduza a sua Password: ");//pede pass
+  fgets(pw,20,stdin);
+  pw[strcspn(pw, "\n")] = 0;
+  strcat(buffer,login);//junta login+pass numa str
+  strcat(buffer," ");
+  strcat(buffer,pw);
+  write(fd, buffer, strlen(buffer));//envia ao servidor
+  memset(buffer,0,strlen(buffer));
+  nread = read(fd, buffer, BUF_SIZE-1); //recebe verificação do login pelo servidor
+  buffer[nread] = '\0';
+	check=strcmp(buffer,"0");
+  if (check==0){
+    printf("Utilizador não existe.Será redirecionado em 5 segundos");
+    printf("\n");
+    sleep(5);
+    frontPAGE(fd); 
+  }
+	memset(buffer,0,strlen(buffer));
+  menu(fd,login);
+}
+
 void menu(int fd,char login[20]){
   system("clear");
   printf("Bem vindo %s à aplicação do Profissional de Saude\n", login);
-  printf("\n1-Fazer Denuncia \n2- Alertar Segurança(Alarme)\n3- Alterar Conta\n4-Menu Ajuda\n0- Sair\n");
+  printf("\n1- Fazer Denuncia \n2- Alertar Segurança(Alarme)\n3- Alterar Conta\n4- Menu Ajuda\n0- Sair\n");
    printf("Escolha uma da opções: ");
 	scanf("%d", &op);
   while(op >4 || op<0){
     printf("Escolha Inválida. Reintroduza: ");
     scanf("%d", &op);
+    getchar();
   }
-
-
 }
 
 void signUP(int fd){
-	char newlogin[20];
-	char newpw[20];
-	printf("\nInsira o Login pretendido: ");
-	scanf("%s", newlogin);
-	printf("\nInsira a password desejada: ");
-	scanf("%s", newpw);
-	strcat(buffer, newlogin);
-	strcat(buffer, " ");
-	strcat(buffer, newpw);
-	write(fd, buffer, BUFF_SIZE-1);
-	memset(buffer, 0, strlen(Biffer));
-	nread = read(fd, buffer, BUFF_SIZE-1);
-	buffer[nread] = '\0';
+  printf("ola)");
+  
 }
 void erro(char *msg){
 	printf("Erro: %s\n", msg);
