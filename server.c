ccf80186bd0faef1deb33fc25874243a390fa2ac
tiagobaltazar <79513158+tiagobaltazar@users.noreply.github.com>
2021-05-01 13:28:56 +0100

    Update server.c

--- server.c
+++ server.c
@@ -116,6 +116,17 @@ void health_app(int client_fd){
 
   
   fclose(text);
+	//criar conta nova
+	/*FILE *fp;
+	nread = read(client_fd, buffer, BUF_SIZE-1);
+	buffer[nread] = '\0';
+	fp = fopen("Health_sign_up.txt", "w");
+	if(fp == NULL)
+		erro("Abertura do ficheiro");
+	char newuser[50];
+	strcpy(newuser, buffer);
+	fprintf(fp, newuser);
+	fclose(fp)*/
 }
 
 void security_app(int client_fd){
