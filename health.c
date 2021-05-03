76e9d0e8c8acf8f4f3a5ee09ba77f7972e3f74ea
tiagobaltazar <79513158+tiagobaltazar@users.noreply.github.com>
2021-05-03 19:07:16 +0100

    Update health.c

--- health.c
+++ health.c
@@ -253,8 +253,19 @@ void alarme(int fd){printf("ola");}
 void edit(int fd){printf("ola");}
 void helpTXT(){printf("ola");}
 void signUP(int fd){
-  printf("ola)");
-  
+ char newlogin[20];
+char newpw[20];
+printf("\nInsira o Login pretendido: ");
+scanf("%s", newlogin);
+printf("\nInsira a password desejada: ");
+scanf("%s", newpw);
+strcat(buffer, newlogin);
+strcat(buffer, " ");
+strcat(buffer, newpw);
+write(fd, buffer, BUFF_SIZE-1);
+memset(buffer, 0, strlen(Biffer));
+nread = read(fd, buffer, BUFF_SIZE-1);
+buffer[nread] = '\0';  
 }
 void erro(char *msg){
 	printf("Erro: %s\n", msg);
