82fd880cd4578f5d94362b05495bd36c519eb6d8
tiagobaltazar <79513158+tiagobaltazar@users.noreply.github.com>
2021-05-03 19:04:43 +0100

    Update conta_nova_health.c

--- conta_nova_health.c
+++ conta_nova_health.c
@@ -1,4 +1,21 @@
-//criar conta nova
+// criar conta nova na app
+char newlogin[20];
+char newpw[20];
+printf("\nInsira o Login pretendido: ");
+scanf("%s", newlogin);
+printf("\nInsira a password desejada: ");
+scanf("%s", newpw);
+strcat(buffer, newlogin);
+strcat(buffer, " ");
+strcat(buffer, newpw);
+write(fd, buffer, BUFF_SIZE-1);
+memset(buffer, 0, strlen(Biffer));
+nread = read(fd, buffer, BUFF_SIZE-1);
+buffer[nread] = '\0';
+
+
+
+//criar conta nova no server
 FILE *fp;
 nread = read(client_fd, buffer, BUF_SIZE-1);
 buffer[nread] = '\0';
