e2265ef2a6a79ff20a2a33d5aa0eea88d1099835
tiagobaltazar <79513158+tiagobaltazar@users.noreply.github.com>
2021-05-06 11:12:12 +0100

    Update admin_update.c

--- admin_update.c
+++ admin_update.c
@@ -122,6 +122,7 @@ void menu(int fd,char login[20]){
 }
 
 void accountcheck(int fd){
+  system("clear");
   char option[]="check";
   write(fd, option, strlen(option));
   //check conta na app de admin
@@ -132,32 +133,35 @@ void accountcheck(int fd){
 	  printf("Opção inválida! Introduza outra vez: ");
   	scanf("%d", &opt);
   }
+  system("clear");
+  memset(buffer, 0, strlen(buffer));
   if(opt == 1){
 	  strcpy(buffer, "1");
   }
   if(opt == 2){
 	  strcpy(buffer, "2");
   }
-  memset(buffer, 0, BUF_SIZE );
-  write(fd, op, BUF_SIZE-1);
+  write(fd, buffer, BUF_SIZE-1);
   memset(buffer, 0, strlen(buffer));
   printf("\nContas novas:\n");
   nread = read(fd, buffer, BUF_SIZE-1);
-	buffer[nread] = '\0';
-	  printf("%s", buffer);
-	  int o = 0;
-	  printf("\nConfirmar contas?\n1 - Sim\n2 - Não");
+  buffer[nread] = '\0';
+  printf("%s", buffer);
+  int o = 0;
+  printf("\nConfirma conta?\n1 - Sim\n2 - Não\nEscolha uma opção: ");
+  scanf("%d", &o);
+  while(o > 2 || o < 1){
+	  printf("Opção inválida! Introduza outra vez: ");
 	  scanf("%d", &o);
-	  while(o > 2 || o < 1){
-		  printf("Opção inválida! Introduza outra vez: ");
-		  scanf("%d", &o);
-	  }
-	  if(o == 1){
-		  strcpy(buffer, "1");
-    }
-    memset(buffer, 0, BUF_SIZE-1);
-    write(fd, buffer BUF_SIZE-1);
-  }  
+  }
+  if(o == 1){
+	  strcpy(buffer, "1");
+  }
+  memset(buffer, 0, BUF_SIZE );
+  write(fd, buffer, BUF_SIZE-1);
+  memset(buffer, 0, BUF_SIZE );
+  frontPAGE(fd);
+}
 void edit(int fd){
   printf("ola");
 }
