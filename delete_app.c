0caf96c890b4c40dd8d1c27ba132580545f6c8b8
rBidault <81481720+rBidault@users.noreply.github.com>
2021-05-06 15:46:51 +0100

    Delete delete_app.c

--- delete_app.c
+++
@@ -1,8 +0,0 @@
-void delete(int fd){
-  memset(buffer, 0, strlen(buffer));
-  strcat(buffer, "delete");
-  write(fd, buffer, BUF_SIZE-1);
-  memset(buffer, 0, strlen(buffer));
-  nread = read(fd, buffer, BUF_SIZE-1);
-  buffer[nread] = '\0';
-}
